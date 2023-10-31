4c4d6e64da002392bf69df465a9269711ecb7f5c
@koudilfan <nouserdetecteditsme@gmail.com>
2023-10-31 09:57:01 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:/Users/win/Desktop/projects/taiwilnd2/public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
